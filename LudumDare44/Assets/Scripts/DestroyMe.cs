40cdaeefcf2ec9e4982a8d3d993f63ccefb16169
cchudac <cchudac@gmail.com>
2019-04-27 14:12:16 +0200

    MoneyType now exists. Destroy- classes are new here.

---
+++ LudumDare44/Assets/Scripts/DestroyMe.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DestroyMe : MonoBehaviour
+{
+	public virtual int SleepTime { get; protected set; }
+	public abstract void Run();
+}
