44552892b33e78585a49ce1e5b5e9b54a62ec465
maoap1 <maoap1@gmail.com>
2019-04-28 17:32:22 +0200

    sounds

---
+++ LudumDare44/Assets/Scripts/MalostranskeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MalostranskeController : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    public AudioSource source;
+    public float newClip;
+    public float timer;
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= newClip + 1)
+        {
+            MakeNewClip();
+            timer = 0;
+        }
+    }
+
+    void MakeNewClip()
+    {
+        int clipNum = Random.Range(0, clips.Length);
+        if (!source.isPlaying)
+        {
+            source.loop = true;
+            source.PlayOneShot(clips[clipNum]);
+        }
+        newClip = clips[clipNum].length;
+    }
+}
