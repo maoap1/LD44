38df3b32ab5f14dfe17416739b0077dec2be2b64
maoap1 <maoap1@gmail.com>
2019-04-29 14:46:47 +0200

    Vylepseny tutorial

--- LudumDare44/Assets/Scripts/ButtonsClick.cs
+++ LudumDare44/Assets/Scripts/ButtonsClick.cs
@@ -13,4 +13,6 @@ public class ButtonsClick : MonoBehaviour
         SceneManager.LoadScene("Malostranske1");
     }
 	public void LegendButton() => SceneManager.LoadScene("LegendScene");
+	public void Legend2Button() => SceneManager.LoadScene("LegendScene2");
+    public void BackToMainManuButton() => SceneManager.LoadScene("MainMenu");
 }
